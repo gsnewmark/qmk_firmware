4cb687ecefb81a2e47dee468d7fd41707ccecdea
Ivan Kryvoruchko <gildraug@gmail.com>
2019-07-23 12:41:23 +0300

    Add dedicated keys for `()` in HotDox keymap

--- keyboards/hotdox/keymaps/gsnewmark/keymap.c
+++ keyboards/hotdox/keymaps/gsnewmark/keymap.c
@@ -16,8 +16,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   =    |   1  |   2  |   3  |   4  |   5  |Break |           | PrScr|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Del   |   Q  |   W  |   E  |   R  |   T  | Space|           | Space|   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |  Del   |   Q  |   W  |   E  |   R  |   T  |Shift/|           |Shift/|   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|  (   |           |   )  |------+------+------+------+------+--------|
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
  * |--------+------+------+------+------+------|  {   |           |   }  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |  [   |           |   ]  |   N  |   M  |   ,  |   .  |   /  | RShift |
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_EQL,         KC_1,         KC_2,     KC_3,   KC_4,   KC_5,   KC_PAUS,
-        KC_DELT,        KC_Q,         KC_W,     KC_E,   KC_R,   KC_T,   KC_SPC,
+        KC_DELT,        KC_Q,         KC_W,     KC_E,   KC_R,   KC_T,   KC_LSPO,
         CTL_T(KC_ESC),  KC_A,         KC_S,     KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,     KC_C,   KC_V,   KC_B,   KC_LBRC,
         LT(SYMB,KC_GRV),KC_LGUI,      KC_LALT,  KC_LEFT,KC_DOWN,
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         KC_SPC, KC_BSPC, KC_END,
         // right hand
              KC_PSCR,     KC_6,   KC_7,    KC_8,   KC_9,   KC_0,             KC_MINS,
-             KC_SPC,      KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,             KC_BSLS,
+             KC_RSPC,     KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,    KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
              KC_RBRC,     KC_N,   KC_M,    KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                   KC_UP,   KC_RGHT,KC_MINS,KC_EQL,          LT(SYMB,KC_GRV),
