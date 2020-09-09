f61733b4bba6cc0ef80afe5793fba14ad929334a
Ivan Kryvoruchko <gsnewmark@pm.me>
2020-09-09 16:26:44 +0300

    Swap media controls and Home/End/PgUp/PgDw in Planck layout

--- keyboards/planck/keymaps/gsnewmark/keymap.c
+++ keyboards/planck/keymaps/gsnewmark/keymap.c
@@ -105,15 +105,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |PrScr |Break | Vol- | Vol+ |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Play | Mute | Prev | Next |
+ * |      |      |      |      |      |             |      | Home | PgDw | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,  KC_LPRN,   KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,  KC_LCBR,   KC_RCBR, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,    KC_PAUS,  KC_VOLD,   KC_VOLU, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MPLY,  KC_MUTE,   KC_MPRV, KC_MNXT
-
+    _______, _______, _______, _______, _______, _______, _______, _______,    KC_HOME,  KC_PGDOWN, KC_PGUP, KC_END
 ),
 
 /* Raise
@@ -124,14 +123,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |PrScr |Break |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Home | PgDw | PgUp | End  |
+ * |      |      |      |      |      |             |      | Play | Mute | Prev | Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,      KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC,   KC_RBRC, GUI_T(KC_BSLS),
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_PAUS, _______,   _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MUTE,   KC_MPRV, KC_MNXT
 ),
 
 /* Plover layer (http://opensteno.org)
